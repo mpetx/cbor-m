05c61d8e77f8f61ad05d5a5ab80165cebf820eb4
mpetx <143336693+mpetx@users.noreply.github.com>
2025-01-02 20:55:41 +0900

    コードを整理

--- src/decode.rs
+++ src/decode.rs
@@ -1,6 +1,75 @@
 
 use crate::event::*;
 
+#[derive(Clone, Copy, PartialEq, Debug)]
+struct Head<'a> {
+    initial_byte: u8,
+    following_bytes: &'a [u8]
+}
+
+impl<'a> Head<'a> {
+
+    const MAJOR_TYPE_MASK: u8 = 0xE0;
+    const ADDITIONAL_INFORMATION_MASK: u8 = 0x1F;
+
+    fn new(initial_byte: u8, following_bytes: &'a [u8]) -> Head<'a> {
+	Head {
+	    initial_byte,
+	    following_bytes
+	}
+    }
+
+    fn major_type(&self) -> u8 {
+	self.initial_byte & Self::MAJOR_TYPE_MASK
+    }
+
+    fn additional_information(&self) -> u8 {
+	self.initial_byte & Self::ADDITIONAL_INFORMATION_MASK
+    }
+
+    fn argument(&self) -> Option<u64> {
+	if !self.is_sound() {
+	    return None;
+	}
+
+	let ai = self.additional_information();
+
+	match ai {
+	    0..24 => Some(ai as u64),
+	    31 => None,
+	    _ => {
+		let mut arg = 0_u64;
+
+		for &b in self.following_bytes {
+		    arg = (arg << 8) | (b as u64);
+		}
+
+		Some(arg)
+	    }
+	}
+    }
+    
+    fn is_sound(&self) -> bool {
+	match self.additional_information() {
+	    0..24 => self.following_bytes.len() == 0,
+	    24 => self.following_bytes.len() == 1,
+	    25 => self.following_bytes.len() == 2,
+	    26 => self.following_bytes.len() == 4,
+	    27 => self.following_bytes.len() == 8,
+	    28 | 29 | 30 => false,
+	    31 => match self.major_type() {
+		0x40 | 0x60 | 0x80 | 0xA0 | 0xE0 => true,
+		0x00 | 0x20 | 0xC0 => false,
+		_ => panic!("unreachable")
+	    },
+	    _ => panic!("unreachable")
+	}
+    }
+    
+}
+
+impl<'a> Eq for Head<'a> {}
+
 pub struct Decoder<'a> {
     data: &'a [u8],
     failed: bool
@@ -15,7 +84,7 @@ impl<'a> Decoder<'a> {
 	}
     }
 
-    pub fn decode_head(&mut self) -> Result<Head<'a>, ()> {
+    fn decode_head(&mut self) -> Result<Head<'a>, ()> {
 	if self.failed || self.data.is_empty() {
 	    self.failed = true;
 	    return Err(());
@@ -40,14 +109,14 @@ impl<'a> Decoder<'a> {
 	if self.data.len() >= following_bytes_len {
 	    let following_bytes = &self.data[0..following_bytes_len];
 	    self.data = &self.data[following_bytes_len..];
-	    Ok(Head { initial_byte, following_bytes })
+	    Ok(Head::new(initial_byte, following_bytes))
 	} else {
 	    self.failed = true;
 	    Err(())
 	}
     }
 
-    pub fn decode_bytes(&mut self, count: usize) -> Result<&'a [u8], ()> {
+    fn decode_bytes(&mut self, count: usize) -> Result<&'a [u8], ()> {
 	if self.failed || self.data.len() < count {
 	    self.failed = true;
 	    return Err(());
@@ -134,6 +203,44 @@ impl<'a> Decoder<'a> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    
+    #[test]
+    fn test_head() {
+	let head = Head::new(0x15, &[]);
+	assert_eq!(head.initial_byte, 0x15);
+	assert_eq!(head.major_type(), 0x00);
+	assert_eq!(head.additional_information(), 0x15);
+	assert_eq!(head.following_bytes, []);
+	assert!(head.is_sound());
+
+	let head = Head::new(0x58, &[0xFB]);
+	assert_eq!(head.initial_byte, 0x58);
+	assert_eq!(head.major_type(), 0x40);
+	assert_eq!(head.additional_information(), 0x18);
+	assert_eq!(head.following_bytes, [0xFB]);
+	assert!(head.is_sound());
+    }
+
+    #[test]
+    fn test_head_argument() {
+	let head = Head::new(0x31, &[]);
+	assert_eq!(head.argument(), Some(0x11));
+
+	let head = Head::new(0x58, &[0xAB]);
+	assert_eq!(head.argument(), Some(0xAB));
+
+	let head = Head::new(0x79, &[0xA5, 0xE6]);
+	assert_eq!(head.argument(), Some(0xA5E6));
+
+	let head = Head::new(0x9A, &[0x98, 0x36, 0x96, 0x0D]);
+	assert_eq!(head.argument(), Some(0x9836_960D));
+
+	let head = Head::new(0xBB, &[0x53, 0x84, 0xC4, 0x60, 0xFD, 0xB0, 0x04, 0xC4]);
+	assert_eq!(head.argument(), Some(0x5384_C460_FDB0_04C4));
+
+	let head = Head::new(0xFF, &[]);
+	assert_eq!(head.argument(), None);
+    }
 
     #[test]
     fn test_decode_head() {
--- src/encode.rs
+++ src/encode.rs
@@ -16,13 +16,8 @@ impl<W: Write> Encoder<W> {
     pub fn new(writer: W) -> Encoder<W> {
 	Encoder { writer }
     }
-
-    pub fn encode_head(&mut self, head: &Head) -> Result<()> {
-	write_u8(&mut self.writer, head.initial_byte)?;
-	self.writer.write_all(head.following_bytes)
-    }
-
-    pub fn encode_head_with_argument(&mut self, major_type: u8, argument: u64) -> Result<()> {
+    
+    fn encode_head_with_argument(&mut self, major_type: u8, argument: u64) -> Result<()> {
 	if argument < 24 {
 	    write_u8(&mut self.writer, major_type | (argument as u8))
 	} else if argument <= 0xFF {
@@ -59,7 +54,7 @@ impl<W: Write> Encoder<W> {
 	}
     }
     
-    pub fn encode_bytes(&mut self, bytes: &[u8]) -> Result<()> {
+    fn encode_bytes(&mut self, bytes: &[u8]) -> Result<()> {
 	self.writer.write_all(bytes)
     }
 
@@ -115,20 +110,6 @@ impl<W: Write> Encoder<W> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_encode_head() {
-	let mut buf = Vec::<u8>::new();
-	let mut enc = Encoder::new(&mut buf);
-	
-	let head = Head::new(0x04, &[]);
-	let _ = enc.encode_head(&head);
-
-	let head = Head::new(0x39, &[0xF9, 0x0D]);
-	let _ = enc.encode_head(&head);
-
-	assert_eq!(buf, [0x04, 0x39, 0xF9, 0x0D]);
-    }
 
     #[test]
     fn test_encode_head_with_argument() {
